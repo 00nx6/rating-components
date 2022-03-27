82009aba18634f5832138aa4acccced0c37ae696
00nx6 <lszabo958@gmail.com>
2022-03-27 15:37:02 +0200

    commit

--- script.js
+++ script.js
@@ -7,15 +7,43 @@ get its data key and add it rating variable
 
 when clicked submit, export selected value
 */ 
+// grabbing containers
+const preCont = document.querySelector('.pre');
+const postCont = document.querySelector('.post');
 
-const bttnsCont = document.querySelector('.buttonCont');
-const bttns = Array.from(bttnsCont.children)
+const backBttn = document.querySelector('.postStar')
+const review = document.querySelector('.userFeedback')
+
+// assigning rating feedback + some style stuff
+const bttns = Array.from(document.querySelectorAll('.bttn'));
+const submitBttn = document.querySelector('.bttnSubmit');
+let serviceRating = 0;
+// removes submit bttn
 bttns.pop()
+
 bttns.forEach(bttn => {
     bttn.addEventListener('click', classToggle)
-});
+})
 
 function classToggle() {
+    // if one of the buttons already has a selected class, remove it
+    bttns.forEach(bttn => {
+        if (bttn.classList.contains('selected')) bttn.classList.remove('selected')
+    })
+    // adds selected class to current bttn
     this.classList.add('selected');
-    
+    // saves the value of current dataset
+    serviceRating = this.dataset.key
+
 }
+
+
+submitBttn.addEventListener('click', submit);
+backBttn.addEventListener('click', submit)
+
+function submit() {
+    preCont.classList.toggle('hidden');
+    postCont.classList.toggle('hidden');
+    review.innerText = `You selected ${serviceRating} out of 5`
+}
+
