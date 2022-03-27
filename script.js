e31fc7d3d7d2b84326294db6e1695da02adbd478
00nx6 <lszabo958@gmail.com>
2022-03-27 15:41:55 +0200

    commit

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-/*
-grab the buttons
-when clicked, add class "active"
-when clicked somewhere else, remove clicked class
-after clicked, check which has click
-get its data key and add it rating variable
 
-when clicked submit, export selected value
-*/ 
 // grabbing containers
 const preCont = document.querySelector('.pre');
 const postCont = document.querySelector('.post');
